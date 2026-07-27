971c35d0de8518093c9a423268dfdd82eb1875be
anishshete21-beep <anishshete21@gmail.com>
2026-07-27 18:27:17 +0530

    Testing pre-commit hook

---
+++ demo.py
@@ -0,0 +1,11 @@
+def greet_users(names):
+    """Prints a friendly greeting for each person in the list."""
+    for name in names:
+        print(f"Hello, {name}! Welcome to Python.")
+
+
+# List of names
+devops_team = ["Anish", "Alice", "Bob"]
+
+# Run the function
+greet_users(devops_team)
